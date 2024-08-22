18f9d0939c09b4a44719f255e07cb9bcb46db2b7
bidis <nikcharg4@gmail.com>
2024-08-22 11:32:42 +0300

    add documentation

--- src/dist/elements.js
+++ src/dist/elements.js
@@ -1,5 +1,10 @@
-"use strict";
-//all elements needed are here
+"use strict"; // Enforce strict mode to catch common coding errors and "unsafe" actions.
+
+// Selecting the element that will display the final result of the calculation.
 const resultElement = document.querySelector(".result");
+
+// Selecting the element that will display the live or intermediate result as the user inputs data.
 const liveResult = document.querySelector(".live-result");
+
+// Selecting all elements with the class "btn", which are assumed to be the calculator's operator buttons.
 const operatorsButton = document.querySelectorAll(".btn");
